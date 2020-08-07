cda85bc19f6c4ba2c4016e09baff7b972f777784
Ahmed NACHID <anachid@e3r12p14.42.fr>
2020-08-07 16:25:47 +0200

    ajout des ex00 > 07

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anachid <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/06 18:30:08 by anachid           #+#    #+#             */
+/*   Updated: 2020/08/07 15:34:01 by anachid          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print(int a, int b)
+{
+	ft_putchar(a / 10 + '0');
+	ft_putchar(a % 10 + '0');
+	write(1, " ", 2);
+	ft_putchar(b / 10 + '0');
+	ft_putchar(b % 10 + '0');
+	if (a != 98)
+		write(1, ", ", 3);
+}
+
+void	ft_print_comb2(void)
+{
+	int a;
+	int b;
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			print(a, b++);
+		}
+		a++;
+	}
+}
