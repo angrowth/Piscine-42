8fdae03ceec04a6fcf4912a37c72a292a2e08b06
Ahmed NACHID <anachid@e3r3p8.42.fr>
2020-08-09 16:44:26 +0200

    cor

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: anachid <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/06 18:30:08 by anachid           #+#    #+#             */
-/*   Updated: 2020/08/09 09:54:02 by anachid          ###   ########.fr       */
+/*   Updated: 2020/08/09 15:36:35 by anachid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,29 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	print(int a, int b)
-{
-	ft_putchar(a / 10 + '0');
-	ft_putchar(a % 10 + '0');
-	write(1, " ", 2);
-	ft_putchar(b / 10 + '0');
-	ft_putchar(b % 10 + '0');
-	if (a != 98)
-		write(1, ", ", 3);
-}
-
 void	ft_print_comb2(void)
 {
-	int	a;
-	int	b;
+	int	i;
+	int	j;
 
-	a = 0;
-	while (a <= 98)
+	i = 0;
+	while (i <= 98)
 	{
-		b = a + 1;
-		while (b <= 99)
+		j = i + 1;
+		while (j <= 99)
 		{
-			print(a, b++);
+			ft_putchar((i / 10) + 48);
+			ft_putchar((i % 10) + 48);
+			ft_putchar(' ');
+			ft_putchar((j / 10) + 48);
+			ft_putchar((j % 10) + 48);
+			if (i != 98 || j != 99)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			j++;
 		}
-		a++;
+		i++;
 	}
 }
